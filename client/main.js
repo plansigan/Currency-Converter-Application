e248f840884cb5e3134d28323477393a1ce2d057
Paolo Luis G. Lansigan <kemchii.69@gmail.com>
2018-09-13 17:30:55 +0800

    finished applying multiple toConvert elements and functionality

--- client/main.js
+++ client/main.js
@@ -6,7 +6,11 @@ var app = new Vue({
         convertedAmount:0,
         convertedCurrency:'AFN',
         countries:{},
-        countriesToConvert:{},
+        countriesToConvert:[
+            //default item
+            { amount: 0, currencyId: 'AFN' },
+            { amount: 0, currencyId: 'XCD' }
+        ],
         formula:{}
     },
     mounted() {
@@ -16,34 +20,65 @@ var app = new Vue({
         this.onSelectCountry() 
     },
     methods:{
-        // naming conventions in functions will be as simple as possible
+        // naming conventions in functions will be as simple as possible so that I don't have to provide a comment.
         getCountries(){
             API.get('countries').then(
                 response => this.countries = response.data.results
             )
         },
-        addCountriesToConvert(){
-
+        addCountry(){
+            var elem = document.createElement('tr')
+            this.countriesToConvert.push({
+                amount:0,
+                currencyId:"AFN"
+            })
+        },
+        removeCountry(index){
+            this.countriesToConvert.splice(index,1)
         },
         calculateCurrency(){
-            var countries = this.selectedCurrency + "_" + this.convertedCurrency
-            this.convertedAmount = this.selectedAmount * this.formula[countries].val
+            this.countriesToConvert = this.countriesToConvert.map(country => {
+                var temp = Object.assign({}, country)
+                var method = this.selectedCurrency + "_" + temp.currencyId
+                temp.amount = this.selectedAmount * this.formula[method].val
+
+                return temp;
+            })
         },
         onSelectCountry(){
             // getting the formula for the currencies
-            var apiValue = "convert?q=" + this.selectedCurrency + "_" + this.convertedCurrency   
-            API.get(apiValue).then(
-                response => this.formula = response.data.results
-            )
-            setTimeout(()=>{
-                this.calculateCurrency()
-            },500)
+            //creating a joined formula for the api is quite hard... quite.
+            var joinedString = this.countriesToConvert.map((country)=>{
+                return  this.selectedCurrency + "_" + country.currencyId
+            }).join(",")
+
+            var apiValue = "convert?q=" + joinedString
+
+            //I use the might of google on this one. I never get used to regex T_T
+            //just making some adjustments because of the limits of the api. I need to hack on this.
+            if (this.isOdd(joinedString.split(",").length)){
+                var arrayOfCountries = joinedString.match(/[^,]+/g);
+            } else {
+                var arrayOfCountries = joinedString.match(/[^,]+,[^,]+/g);
+            }
+            
+            // so here I loop through the api and combine it in a object all together to this.formula
+            for (let countries of arrayOfCountries ){
+                var apiValue = "convert?q=" + countries
+                API.get(apiValue).then(
+                    response => this.formula = Object.assign(response.data.results, this.formula)
+                ).then(()=>{
+                    this.calculateCurrency()
+                })
+            }
+        },
+        isOdd(num){
+            return num % 2;
         }
     },
     watch:{
         selectedAmount(){
-            var countries = this.selectedCurrency + "_" + this.convertedCurrency
-            this.convertedAmount = this.selectedAmount * this.formula[countries].val
+            this.calculateCurrency()
         }
     }
 })
